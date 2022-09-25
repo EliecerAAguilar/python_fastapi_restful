fde24718a0493e7c367c7ee0fdac22e662c2cfb6
Eliecer <eliecer.aguilar1@utp.ac.pa>
2022-09-24 23:25:24 -0500

    save data using POST method and showing with GET method

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
+from models.models import Post
 
 app = FastAPI()
 
-
+posts = []
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+@app.get("/posts")
+async def root():
+    return posts
 
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@app.post('/posts')
+def save_posts(post: Post):
+    posts.append(post.dict())
+    return "recived"
---
+++ models/models.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from typing import Text, Optional
+from datetime import datetime
+
+
+class Post(BaseModel):
+    id: Optional[str]
+    title: str
+    author: str
+    content: Text
+    created_at: datetime = datetime.now()
+    published_at: Optional[datetime]
+    published: bool = False
