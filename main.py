7ccd5a2118b37afd5d922a6669ddfd6b7194866a
Eliecer <eliecer.aguilar1@utp.ac.pa>
2022-09-25 01:05:10 -0500

    update nethod working with http responses

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, status, HTTPException
 from models.models import Post
 from uuid import uuid4 as uuid
+from fastapi.responses import JSONResponse
 
 
 app = FastAPI()
@@ -20,7 +21,11 @@ async def show_posts():
 async def save_posts(post: Post):
     post.id = str(uuid())
     posts.append(post.dict())
-    return status.HTTP_201_CREATED
+    return JSONResponse(
+        status_code=status.HTTP_201_CREATED,
+        content={"message": "Post Created successfully"}
+    )
+    # return {"detail": "Post Created successfully", "status_code": status.HTTP_201_CREATED}
 
 
 @app.get('/posts/{post_id}')
@@ -36,7 +41,24 @@ async def delete_post(post_id: str):
     for index, post in enumerate(posts):
         if post["id"] == post_id:
             posts.pop(index)
-            return {"detail": "Post has been deleted successfully", "status_code": status.HTTP_200_OK}
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content={"detail": "Post has been deleted successfully"}
+            )
+            # return {"detail": "Post has been deleted successfully", "status_code": status.HTTP_200_OK}
     return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
 
 
+@app.put("/posts/{post_id}")
+async def update_post(post_id:str, update_post:Post):
+    for index, post in enumerate(posts):
+        if post["id"] == post_id:
+            posts[index]["title"] = update_post.title
+            posts[index]["content"] = update_post.content
+            posts[index]["author"] = update_post.author
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content={"detail": "Post has been updated successfully"}
+            )
+            # return {"detail": "Post has been updated successfully", "status_code": status.HTTP_200_OK}
+    return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
