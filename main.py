946920893ff4da6f606ef87b40f375d2601120ce
Eliecer <eliecer.aguilar1@utp.ac.pa>
2022-09-25 00:35:07 -0500

    method delete working

--- main.py
+++ main.py
@@ -28,5 +28,15 @@ async def get_post(post_id: str):
     for post in posts:
         if post["id"] == post_id:
             return post
-    return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Item not found")
+    return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+
+
+@app.delete('/posts/{post_id}')
+async def delete_post(post_id: str):
+    for index, post in enumerate(posts):
+        if post["id"] == post_id:
+            posts.pop(index)
+            return {"detail": "Post has been deleted successfully", "status_code": status.HTTP_200_OK}
+    return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+
 
